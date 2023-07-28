c272269aa74807453548f65c7324512506a0545e
Johan07 <duquejohan7@gmail.com>
2023-07-28 16:59:51 -0500

    Remove unused attribute from state

--- lib/presentation/cubits/marvel_characters/marvel_characters_state.dart
+++ lib/presentation/cubits/marvel_characters/marvel_characters_state.dart
@@ -3,12 +3,10 @@ part of 'marvel_characters_cubit.dart';
 @immutable
 abstract class MarvelCharactersState {
   final List<Character> characters;
-  final bool noMoreData;
   final DioException? error;
 
   const MarvelCharactersState({
     this.characters = const [],
-    this.noMoreData = true,
     this.error,
   });
 }
@@ -18,7 +16,7 @@ class MarvelCharactersLoading extends MarvelCharactersState {
 }
 
 class MarvelCharactersSuccess extends MarvelCharactersState {
-  const MarvelCharactersSuccess({super.characters, super.noMoreData});
+  const MarvelCharactersSuccess({super.characters});
 }
 
 class MarvelCharactersFailed extends MarvelCharactersState {
