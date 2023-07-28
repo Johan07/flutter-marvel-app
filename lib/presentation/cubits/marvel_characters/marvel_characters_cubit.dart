c5dccdfb9553da825d4f4858e02e564e7f0398da
Johan07 <duquejohan7@gmail.com>
2023-07-28 17:02:22 -0500

    Add marvel character cubit

---
+++ lib/presentation/cubits/marvel_characters/marvel_characters_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:marvel_app/domain/models/character.dart';
+import 'package:marvel_app/domain/models/requests/marvel_characters_request.dart';
+import 'package:marvel_app/domain/models/responses/marvel_characters_response.dart';
+import 'package:marvel_app/domain/repositories/api_repository.dart';
+import 'package:marvel_app/presentation/cubits/base/base_cubit.dart';
+import 'package:marvel_app/utils/resources/data_state.dart';
+import 'package:meta/meta.dart';
+
+part 'marvel_characters_state.dart';
+
+class MarvelCharactersCubit
+    extends BaseCubit<MarvelCharactersState, List<Character>> {
+  final ApiRepository _apiRepository;
+
+  MarvelCharactersCubit(this._apiRepository)
+      : super(MarvelCharactersLoading(), []);
+
+  Future<void> getMarvelCharacters() async {
+    if (isBusy) return;
+
+    await run(() async {
+      final response = await _apiRepository.getMarvelCharacters(
+        request: MarvelCharactersRequest(),
+      );
+      if (response is DataSuccess<MarvelCharactersResponse>) {
+        List<Character> characters = response.data!.data!.results ?? [];
+        data.addAll(characters);
+        emit(MarvelCharactersSuccess(characters: data));
+      } else if (response is DataFailed) {
+        emit(MarvelCharactersFailed(error: response.error));
+      }
+    });
+  }
+}
